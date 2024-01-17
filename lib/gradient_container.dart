85de000f542a685c321b850bae9f37ffff710e41
DMC-08 <646051200079@mail.rmutk.ac.th>
2024-01-17 12:14:45 +0700

    first commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:ze04/stylet.dart';
+
+class GContainer extends StatelessWidget{
+  GContainer({super.key});
+
+  Widget build(context){
+    return Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(221, 77, 197, 224),
+            Color.fromARGB(66, 225, 104, 104)
+          ],begin: Alignment.topRight,end: Alignment.bottomLeft)),
+          child:  Center(
+            child: StyleT()
+          ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ze04/gradient_container.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        body: GContainer()),
+  ));
+}
+
